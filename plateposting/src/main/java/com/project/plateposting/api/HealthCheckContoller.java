5e5e28329a7d1c1f35904958ea91da7f0445b36c
SeokjinDev <tjrwls1108011@gmail.com>
2024-12-03 22:36:35 +0900

    [FIX] HealthCheckController 경로 수정

--- plateposting/src/main/java/com/project/plateposting/api/HealthCheckContoller.java
+++ plateposting/src/main/java/com/project/plateposting/api/HealthCheckContoller.java
@@ -1,4 +1,4 @@
-package com.project.plateposting.api.member;
+package com.project.plateposting.api;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
